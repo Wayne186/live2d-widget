101167563871eefdf17680024697ed70b80f20fc
Wayne186 <wayne186645@gmail.com>
2023-09-19 16:29:47 +0800

    .

--- autoload.js
+++ autoload.js
@@ -1,6 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-//const live2d_path = "/live2d-widget/";
+//const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -27,13 +26,16 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		// loadExternalResource(live2d_path + "waifu.css", "css"),
+		// loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		// loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		loadExternalResource("waifu.css", "css"),
+		loadExternalResource("live2d.min.js", "js"),
+		loadExternalResource("waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
+			waifuPath: "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
