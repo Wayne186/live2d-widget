4b7e00beec8bb219358a8755fc1a808ce46e58e1
Wayne186 <wayne186645@gmail.com>
2023-09-19 16:45:43 +0800

    bgfx

--- autoload.js
+++ autoload.js
@@ -25,18 +25,15 @@ function loadExternalResource(url, type) {
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
-	console.log("try load live2d");
+		console.log("try load live2d");
 	Promise.all([
-		// loadExternalResource(live2d_path + "waifu.css", "css"),
-		// loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		// loadExternalResource(live2d_path + "waifu-tips.js", "js")
-		loadExternalResource("waifu.css", "css"),
-		loadExternalResource("live2d.min.js", "js"),
-		loadExternalResource("waifu-tips.js", "js")
+		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path + "live2d.min.js", "js"),
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
-			waifuPath: "waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
