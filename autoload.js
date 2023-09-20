a2b696f00bb9219bd0f949658f0b49d1acf5b951
Wayne186 <wayne186645@gmail.com>
2023-09-20 15:52:32 +0800

    update

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/Wayne186/live2d-widget@f7c5c56/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/Wayne186/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
